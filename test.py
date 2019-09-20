ccd8c1a8dc187d1d4d3f3d028a49273193b67066
Tony220 <42564472+Tony220@users.noreply.github.com>
2019-09-20 13:07:06 +0300

    обавил несколько функций для строки

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Howdy, children")
+a = "howdy, children"
+print(a)
+a = a.capitalize()
+print(a) # make first character in up case
+a = a.upper()
+print(a)
+a = a.casefold()
+print(a)
+a = a.center()
+print(a)
