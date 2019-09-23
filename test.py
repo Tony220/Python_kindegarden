ec6c857b1082dc7f61b6da7fd0f807583eedf412
Tony220 <42564472+Tony220@users.noreply.github.com>
2019-09-23 22:06:39 +0300

    обавил несколько функций для строки

--- test.py
+++ test.py
@@ -1,25 +1,44 @@
-
+"""
+String methods and functions
+"""
 a = "howdy, children"
 print(a)
-a = a.capitalize()
-print(a) # make first character in up case
-a = a.upper()
+a = a.capitalize() # make first character in up case
 print(a)
-a = a.casefold()
+a = a.upper() #make all txt bigger
 print(a)
-a = a.center(1)
+a = a.casefold() #make all txt lower
 print(a)
-c  = a.count("h")
+a = a.center(4,"b") #center obj-s
+print(a)
+c  = a.count("h") #count accurate obj-s
 print(c)
-byte = a.encode()
+byte = a.encode() #code line in byte
 print(byte)
-bool = a.endswith("n")
+bool = a.endswith("n") #reserch which symbol end the line
 print(bool)
-a = a.expandtabs(5)
+a = a.expandtabs(5) #share tab
 print(a)
-f = a.find("d")
+f = a.find("d") #return elem id
 print(f)
 sharestr = "In phrases '{}' are {} h"
-print(sharestr.format(a, c))
-a = a.format_map(2)
+print(sharestr.format(a, c)) # isn't easy format print(f"text {} {}") is easier
+"""str = "howdy, children {pla}"
+str.format_map(pla=" in tajikistan")
+print(str) """ #TODO I don't know how it works
+bool = isinstance(a, str) #check format of obj-t
+print(bool)
+f = a.index("h") #the same thng that (a.find)
+print(f)
+a = a.join(" 1")
 print(a)
+a = a.translate("russian")
+print(a)
+"""
+List methods and functions
+"""
+my_list = ["Howdy", "children"]
+my_list.append(". You are loozers!")
+print(my_list)
+my_list.reverse()
+print(my_list)
